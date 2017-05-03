6d631621e2274b7a95510af9bb41fd71149fd3f5
Miles Johnson <mileswjohnson@gmail.com>
2017-05-03 00:09:23 -0700

    Move extra files to a new folder.

--- bin/lint.js
+++ bin/lint.js
@@ -4,7 +4,7 @@ const path = require('path');
 process.argv.push(
   '--color',
   '--config=' + path.join(__dirname, '../.eslintrc'),
-  '--ignore-path=' + path.join(__dirname, '../.eslintignore'),
+  '--ignore-path=' + path.join(__dirname, '../.eslintignore')
 );
 
 require('eslint/bin/eslint');
