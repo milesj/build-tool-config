4c696da8b6f3deb56980bb6e6f8b2303ef8bdb22
Miles Johnson <mileswjohnson@gmail.com>
2017-05-02 19:23:16 -0700

    Merged eslint rules.

--- eslint.js
+++ eslint.js
@@ -65,6 +65,23 @@ module.exports = {
       groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
       'newlines-between': 'never',
     }],
+    'react/forbid-foreign-prop-types': 'error',
+    'react/jsx-handler-names': ['error', {
+      eventHandlerPrefix: 'handle',
+      eventHandlerPropPrefix: 'on',
+    }],
+    'react/jsx-key': 'error',
+    'react/jsx-no-literals': 'error',
+    'react/jsx-max-props-per-line': ['error', {
+      maximum: 1,
+      when: 'multiline',
+    }],
+    'react/no-direct-mutation-state': 'error',
+    'react/sort-prop-types': ['error', {
+      ignoreCase: true,
+      callbacksLast: true,
+    }],
+    'react/void-dom-elements-no-children': 'error',
 
     // Flowtype support
     'flowtype/boolean-style': 'error',
