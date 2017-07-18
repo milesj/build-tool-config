dcc3c3aa9ed990a81e8aa54d939293d26c4fc0a6
Miles Johnson <mileswjohnson@gmail.com>
2017-07-17 20:45:51 -0700

    Updated init to be async.

--- bin/init.js
+++ bin/init.js
@@ -5,33 +5,42 @@ const path = require('path');
 const options = require('yargs-parser')(process.argv.slice(2));
 
 const PACKAGE_PATH = path.join(process.cwd(), 'package.json');
-const packageConfig = fs.readFileSync(PACKAGE_PATH, 'utf8');
 const isNode = options.node || false;
 
-packageConfig.babel = {
-  extends: `./node_modules/@milesj/build-tool-config/babel${isNode ? '.node' : ''}.json5`,
-};
-
-packageConfig.eslintConfig = {
-  extends: `./node_modules/@milesj/build-tool-config/eslint${isNode ? '.node' : ''}.json5`,
-};
-
-packageConfig.jest = {
-  preset: '@milesj/build-tool-config',
-};
-
-if (packageConfig.engines) {
-  packageConfig.engines.node = '>=6.5.0';
-} else {
-  packageConfig.engines = {
-    node: '>=6.5.0',
-  };
-}
-
-fs.writeFile(PACKAGE_PATH, JSON.stringify(packageConfig, null, '  '), 'utf8', (error) => {
+fs.readFile(PACKAGE_PATH, 'utf8', (error, data) => {
   if (error) {
-    console.error('Failed to update package.json', error);
-  } else {
-    console.log('Updated package.json');
+    console.error('Failed to read package.json. Please run command from project root.');
+
+    return;
   }
+
+  const packageConfig = JSON.parse(data);
+
+  packageConfig.babel = {
+    extends: `./node_modules/@milesj/build-tool-config/babel${isNode ? '.node' : ''}.json5`,
+  };
+
+  packageConfig.eslintConfig = {
+    extends: `./node_modules/@milesj/build-tool-config/eslint${isNode ? '.node' : ''}.json5`,
+  };
+
+  packageConfig.jest = {
+    preset: '@milesj/build-tool-config',
+  };
+
+  if (packageConfig.engines) {
+    packageConfig.engines.node = '>=6.5.0';
+  } else {
+    packageConfig.engines = {
+      node: '>=6.5.0',
+    };
+  }
+
+  fs.writeFile(PACKAGE_PATH, JSON.stringify(packageConfig, null, 2), 'utf8', (writeError) => {
+    if (writeError) {
+      console.error('Failed to update package.json', writeError);
+    } else {
+      console.log('Updated package.json');
+    }
+  });
 });
