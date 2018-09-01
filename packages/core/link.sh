cb642d3e93d43d5b3b18e06b83de14865e815af6
Miles Johnson <mileswjohnson@gmail.com>
2018-09-01 13:04:56 -0700

    Fix link again.

--- packages/core/link.sh
+++ packages/core/link.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Symlink specific files from the @milesj/build-tool-config package
-ln -s -f "../config/configs" "./configs"
-ln -s -f "../config/dotfiles" "./dotfiles"
-ln -s -f "../config/scripts" "./scripts"
+ln -s -f "../build-tool-config/configs" "./configs"
+ln -s -f "../build-tool-config/dotfiles" "./dotfiles"
+ln -s -f "../build-tool-config/scripts" "./scripts"
