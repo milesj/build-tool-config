6164895287b2ca061c2a16e7c048b373050d676d
Miles Johnson <mileswjohnson@gmail.com>
2019-02-07 23:58:38 -0800

    Fix again.

--- packages/config/scripts/build.js
+++ packages/config/scripts/build.js
@@ -8,35 +8,45 @@ module.exports = class BuildScript extends Script {
   }
 
   buildCjs(context) {
-    return this.handleResponse(
-      this.executeCommand('beemo', ['babel', '--workspaces=*', '--clean'], {
-        cwd: context.root,
-      }),
-    );
+    return this.executeCommand('beemo', ['babel', '--workspaces=*', '--clean'], {
+      cwd: context.root,
+    })
+      .then(response => {
+        this.tool.log(response.stdout);
+
+        return response;
+      })
+      .catch(error => {
+        this.tool.logError(error.message);
+
+        throw error;
+      });
   }
 
   buildEsm(context) {
-    return this.handleResponse(
-      this.executeCommand('beemo', ['babel', '--workspaces=*', '--clean', '--esm'], {
-        cwd: context.root,
-      }),
-    );
+    return this.executeCommand('beemo', ['babel', '--workspaces=*', '--clean', '--esm'], {
+      cwd: context.root,
+    })
+      .then(response => {
+        this.tool.log(response.stdout);
+
+        return response;
+      })
+      .catch(error => {
+        this.tool.logError(error.message);
+
+        throw error;
+      });
   }
 
   buildDeclarations(context) {
-    return this.handleResponse(
-      this.executeCommand(
-        'beemo',
-        ['typescript', '--workspaces=*', '--declaration', '--emitDeclarationOnly'],
-        {
-          cwd: context.root,
-        },
-      ),
-    );
-  }
-
-  handleResponse(promise) {
-    return promise
+    return this.executeCommand(
+      'beemo',
+      ['typescript', '--workspaces=*', '--declaration', '--emitDeclarationOnly'],
+      {
+        cwd: context.root,
+      },
+    )
       .then(response => {
         this.tool.log(response.stdout);
 
