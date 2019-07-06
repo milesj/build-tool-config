383ed4b636c3f21d3a050750dbe3b82b38eea5a9
Miles Johnson <mileswjohnson@gmail.com>
2019-07-06 14:25:06 -0700

    deps(Babel): Updated Babel to v7.5.

--- packages/config/configs/babel.js
+++ packages/config/configs/babel.js
@@ -10,7 +10,6 @@ const plugins = [
   '@babel/plugin-proposal-nullish-coalescing-operator',
   '@babel/plugin-proposal-optional-catch-binding',
   '@babel/plugin-proposal-optional-chaining',
-  '@babel/plugin-syntax-dynamic-import',
   ['babel-plugin-transform-dev', { evaluate: false }],
 ];
 
