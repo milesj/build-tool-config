92adc895d494cd2234308b766190efdc75ca233c
Miles Johnson <mileswjohnson@gmail.com>
2020-12-11 15:43:41 -0800

    Fix symlinking for pnp.

--- packages/core/link.js
+++ packages/core/link.js
@@ -6,10 +6,11 @@ const path = require('path');
 // Symlink specific folders from the @milesj/build-tool-config package
 const links = ['lib', 'templates'];
 
-links.forEach(folder => {
+links.forEach((folder) => {
   try {
     fs.symlinkSync(
-      path.join(__dirname, '../build-tool-config', folder),
+      // Resolves to lib/index.js
+      path.join(require.resolve('@milesj/build-tool-config'), '../..', folder),
       path.join(__dirname, folder),
       'dir',
     );
